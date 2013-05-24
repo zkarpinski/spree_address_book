920504128f173f43959cdc0cb1928245bf538378
Zach Karpinski <zkarpinski@gmail.com>
2013-05-24 16:04:46 -0500

    Using spaces instead of BR tags

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -7,14 +7,14 @@ Spree::Address.class_eval do
     validator = Spree::Address.validators.find{|v| v.kind_of?(ActiveModel::Validations::PresenceValidator)}
     validator ? validator.attributes : []
   end
-  
+
   # TODO: look into if this is actually needed. I don't want to override methods unless it is really needed
   # can modify an address if it's not been used in an order
   def same_as?(other)
     return false if other.nil?
     attributes.except('id', 'updated_at', 'created_at', 'user_id') == other.attributes.except('id', 'updated_at', 'created_at', 'user_id')
   end
-  
+
   # can modify an address if it's not been used in an completed order
   def editable?
     new_record? || (shipments.empty? && Spree::Order.complete.where("bill_address_id = ? OR ship_address_id = ?", self.id, self.id).count == 0)
@@ -32,6 +32,10 @@ Spree::Address.class_eval do
       "#{city}, #{state || state_name} #{zipcode}",
       "#{country}"
     ].reject(&:empty?).join("<br/>").html_safe
+  end
+
+  def to_select
+    to_s.gsub(/<br\/>/, " ")
   end
 
   # UPGRADE_CHECK if future versions of spree have a custom destroy function, this will break
