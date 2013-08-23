b71bb8d8122f12934da5437c5f45d65e031d79ac
Zach Karpinski <zkarpinski@gmail.com>
2013-08-23 10:32:27 -0500

    Conditional polling; Hiding sidebar elements to expand address loader;

--- app/assets/javascripts/admin/spree_address_book.js
+++ app/assets/javascripts/admin/spree_address_book.js
@@ -1,14 +1,30 @@
 //= require store/spree_core
 
-var previousVal;
-var pollInterval = setInterval(function() {
+var admin_edit_cust_details_reg = /\/admin\/orders\/([^\/]+)\/customer/
 
-    var val = $('#order_email').val();
+// Only poll if we're on the right page
+if (admin_edit_cust_details_reg.test(window.location.pathname))
+{
+  $(window).focus(function() { previousVal = ""; });
+  var previousVal;
+  var pollInterval = setInterval(function() {
 
-    if (val != '' && val !== previousVal) {
-      $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val() }).fadeIn(1000); 
-    }
+      var val = $('#order_email').val();
 
-    previousVal = val;
+      if (val != '' && val !== previousVal) {
+        $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val() }).fadeIn(1000); 
+      }
 
-}, 2000);
+      previousVal = val;
+
+  }, 2000);
+
+  function expand_address_loader() {
+    $('#order_tab_summary').slideUp(1000);
+    $('aside nav.menu').slideUp(1000);
+    $('#address-loader').css('max-height', '2000px');
+    $('#address-loader').css('height', 'auto');
+    return false;
+  }
+}
+
