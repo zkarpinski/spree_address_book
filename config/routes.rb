a3ca6b29ffc01d236dc1ebcd3e6acc3a7cf69b3b
Zach Karpinski <zkarpinski@gmail.com>
2013-08-23 10:33:53 -0500

    Address search route; Various Styles;

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Spree::Core::Engine.routes.draw do
 
   namespace :admin do
     post 'addresses/search' => 'addresses#search', as: 'address_search'
+    delete 'addresses/:id' => 'addresses#destroy', as: 'address'
   end
 end
