c929923bda213b9e23bf46800716380c01558ef0
Zach Karpinski <zkarpinski@gmail.com>
2013-09-04 14:18:48 -0500

    Removed some excess jquery

--- app/assets/javascripts/admin/spree_address_book.js
+++ app/assets/javascripts/admin/spree_address_book.js
@@ -1,32 +1,28 @@
-//= require admin/spree_core
 
-(function($) {
-  $(document).ready(function(){
-    var admin_edit_cust_details_reg = /\/admin\/orders\/([^\/]+)\/customer/
+var admin_edit_cust_details_reg = /\/admin\/orders\/([^\/]+)\/customer/
 
-    // Only poll if we're on the right page
-    if (admin_edit_cust_details_reg.test(window.location.pathname))
-    {
-      $(window).focus(function() { previousVal = ""; });
-      var previousVal;
-      var pollInterval = setInterval(function() {
+// Only poll if we're on the right page
+if (admin_edit_cust_details_reg.test(window.location.pathname))
+{
+  $(window).focus(function() { previousVal = ""; });
+  var previousVal;
+  var pollInterval = setInterval(function() {
 
-          var val = $('#order_email').val();
+      var val = $('#order_email').val();
 
-          if (val != '' && val !== previousVal) {
-            $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val() }).fadeIn(1000); 
-          }
-
-          previousVal = val;
-
-      }, 2000);
-
-      function expand_address_loader() {
-        $('#order_tab_summary').slideUp(1000);
-        $('aside nav.menu').slideUp(1000);
-        $('#address-loader').css('max-height', '2000px');
-        $('#address-loader').css('height', 'auto');
-        return false;
+      if (val != '' && val !== previousVal) {
+        $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val() }).fadeIn(1000); 
       }
-    }
-})(jQuery);
+
+      previousVal = val;
+
+  }, 2000);
+
+  function expand_address_loader() {
+    $('#order_tab_summary').slideUp(1000);
+    $('aside nav.menu').slideUp(1000);
+    $('#address-loader').css('max-height', '2000px');
+    $('#address-loader').css('height', 'auto');
+    return false;
+  }
+}
