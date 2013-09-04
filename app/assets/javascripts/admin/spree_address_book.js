823a182ce59f2917b144dec994371c74db523c68
Zach Karpinski <zkarpinski@gmail.com>
2013-09-04 13:40:24 -0500

    Wrapped in Jquery

--- app/assets/javascripts/admin/spree_address_book.js
+++ app/assets/javascripts/admin/spree_address_book.js
@@ -1,30 +1,32 @@
-//= require store/spree_core
+//= require admin/spree_core
 
-var admin_edit_cust_details_reg = /\/admin\/orders\/([^\/]+)\/customer/
+(function($) {
+  $(document).ready(function(){
+    var admin_edit_cust_details_reg = /\/admin\/orders\/([^\/]+)\/customer/
 
-// Only poll if we're on the right page
-if (admin_edit_cust_details_reg.test(window.location.pathname))
-{
-  $(window).focus(function() { previousVal = ""; });
-  var previousVal;
-  var pollInterval = setInterval(function() {
+    // Only poll if we're on the right page
+    if (admin_edit_cust_details_reg.test(window.location.pathname))
+    {
+      $(window).focus(function() { previousVal = ""; });
+      var previousVal;
+      var pollInterval = setInterval(function() {
 
-      var val = $('#order_email').val();
+          var val = $('#order_email').val();
 
-      if (val != '' && val !== previousVal) {
-        $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val() }).fadeIn(1000); 
+          if (val != '' && val !== previousVal) {
+            $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val() }).fadeIn(1000); 
+          }
+
+          previousVal = val;
+
+      }, 2000);
+
+      function expand_address_loader() {
+        $('#order_tab_summary').slideUp(1000);
+        $('aside nav.menu').slideUp(1000);
+        $('#address-loader').css('max-height', '2000px');
+        $('#address-loader').css('height', 'auto');
+        return false;
       }
-
-      previousVal = val;
-
-  }, 2000);
-
-  function expand_address_loader() {
-    $('#order_tab_summary').slideUp(1000);
-    $('aside nav.menu').slideUp(1000);
-    $('#address-loader').css('max-height', '2000px');
-    $('#address-loader').css('height', 'auto');
-    return false;
-  }
-}
-
+    }
+})(jQuery);
