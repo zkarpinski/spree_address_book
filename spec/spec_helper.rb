54711419ee687f4f20830ecb60130a27b12d4454
Zach Karpinski <zkarpinski@gmail.com>
2013-11-06 13:12:22 -0600

    Updated spec helper so tests will run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,13 +13,18 @@ Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
 
 # Requires factories defined in spree_core
 require 'spree/core/testing_support/factories'
-require 'spree/core/testing_support/env'
+require 'spree/core/testing_support/controller_requests'
+require 'spree/core/testing_support/authorization_helpers'
+require 'spree/core/testing_support/preferences'
+require 'spree/core/testing_support/flash'
 require 'spree/core/url_helpers'
+
 
 RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = false
   config.include Spree::Core::UrlHelpers
+  config.include Devise::TestHelpers, :type => :controller
 
   config.before(:each) do
     if example.metadata[:js]
