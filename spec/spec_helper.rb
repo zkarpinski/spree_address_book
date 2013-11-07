94e1260b9f9abc94552483192b61ee6724ac5082
Zach Karpinski <zkarpinski@gmail.com>
2013-11-06 20:44:39 -0600

    Adding additional testing helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,18 +13,23 @@ Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
 
 # Requires factories defined in spree_core
 require 'spree/core/testing_support/factories'
+require 'spree/core/testing_support/capybara_ext'
 require 'spree/core/testing_support/controller_requests'
 require 'spree/core/testing_support/authorization_helpers'
 require 'spree/core/testing_support/preferences'
 require 'spree/core/testing_support/flash'
 require 'spree/core/url_helpers'
-
+require 'paperclip/matchers'
 
 RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = false
-  config.include Spree::Core::UrlHelpers
   config.include Devise::TestHelpers, :type => :controller
+  config.include FactoryGirl::Syntax::Methods
+  config.include Spree::Core::UrlHelpers
+  config.include Spree::Core::TestingSupport::ControllerRequests
+  config.include Spree::Core::TestingSupport::Preferences
+  config.include Spree::Core::TestingSupport::Flash
 
   config.before(:each) do
     if example.metadata[:js]
