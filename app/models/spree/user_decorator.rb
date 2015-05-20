49b0a6004100aaa246488256e18c990d8d48b754
Zach Karpinski <zkarpinski@gmail.com>
2015-05-20 09:52:01 -0500

    Improved address presentation

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,3 +1,3 @@
 Spree.user_class.class_eval do
-  has_many :addresses, :conditions => {:deleted_at => nil}, :order => "updated_at DESC", :class_name => 'Spree::Address'
+  has_many :addresses, :conditions => {:deleted_at => nil}, :class_name => 'Spree::Address'
 end
