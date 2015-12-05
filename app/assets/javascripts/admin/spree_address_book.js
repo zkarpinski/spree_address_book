b99cfda682ca89673b49b401c2d737bca33e665f
Zach Karpinski <zkarpinski@gmail.com>
2015-12-05 10:47:08 -0600

    Only showing address search in customer details

--- app/assets/javascripts/admin/spree_address_book.js
+++ app/assets/javascripts/admin/spree_address_book.js
@@ -2,49 +2,54 @@
 
 
 $(document).ready(function() {
-  var previousVal = "";
-  var email_timerid;
+  var admin_edit_cust_details_reg = /\/admin\/orders\/([^\/]+)\/customer/;
+  // Only poll if we're on the right page
+  if (admin_edit_cust_details_reg.test(window.location.pathname))
+  {
+    var previousVal = "";
+    var email_timerid;
 
-  var address_timerid;
-  var previousAddressSearch = "";
-  var form = this;
+    var address_timerid;
+    var previousAddressSearch = "";
+    var form = this;
 
-  $('#order_email').keyup(function() {
-    clearTimeout(email_timerid);
+    $('#order_email').keyup(function() {
+      clearTimeout(email_timerid);
 
-    email_timerid = setTimeout(function() {
-      var val = $('#order_email').val();
+      email_timerid = setTimeout(function() {
+        var val = $('#order_email').val();
 
-      if (val !== "" && val !== previousVal) {
-        $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val(), 'a': $('#order_address_search').val() }).fadeIn(1000);
-        previousVal = val;
-      }
-    }, 2000);
-  });
+        if (val !== "" && val !== previousVal) {
+          $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val(), 'a': $('#order_address_search').val() }).fadeIn(1000);
+          previousVal = val;
+        }
+      }, 2000);
+    });
 
 
-  $('#address-loader').on('keyup', '#order_address_search', function() {
-    clearTimeout(address_timerid);
+    $('#address-loader').on('keyup', '#order_address_search', function() {
+      clearTimeout(address_timerid);
 
-    address_timerid = setTimeout(function() {
-      var addressSearch = $('#order_address_search').val();
+      address_timerid = setTimeout(function() {
+        var addressSearch = $('#order_address_search').val();
 
-      if (addressSearch !== previousAddressSearch) {
-        $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val(), 'a': $('#order_address_search').val() }).fadeIn(1000);
-        previousAddressSearch = addressSearch;
-      }
-    }, 2000);
-  });
+        if (addressSearch !== previousAddressSearch) {
+          $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val(), 'a': $('#order_address_search').val() }).fadeIn(1000);
+          previousAddressSearch = addressSearch;
+        }
+      }, 2000);
+    });
 
-  function expand_address_loader() {
-    $('#order_tab_summary').slideUp(1000);
-    $('aside nav.menu').slideUp(1000);
-    $('#address-loader').css('max-height', '2000px');
-    $('#address-loader').css('height', 'auto');
-    return false;
-  }
-  if($('#order_email').val() !== "") {
-    $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val() }).fadeIn(1000);
+    function expand_address_loader() {
+      $('#order_tab_summary').slideUp(1000);
+      $('aside nav.menu').slideUp(1000);
+      $('#address-loader').css('max-height', '2000px');
+      $('#address-loader').css('height', 'auto');
+      return false;
+    }
+    if($('#order_email').val() !== "") {
+      $('#address-loader').load('/admin/addresses/search', {'q': $("#order_email").val() }).fadeIn(1000);
+    }
   }
 });
 
